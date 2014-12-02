3950a33c22b5c9317107a5af64fd9e2cf1cea1f3
Mathias Lykkegaard Lorenzen <mathias.lorenzen@live.com>
2014-12-02 21:10:52 +0100

    new facade for progress updates.

---
+++ Tyranotorrent/Tyranotorrent/MonoTorrent/TorrentManagerObservableFacade.cs
@@ -0,0 +1,39 @@
+﻿using MonoTorrent.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyranotorrent.MonoTorrent
+{
+    class TorrentManagerViewModelFacade : INotifyPropertyChanged
+    {
+        private TorrentManager manager;
+
+        public double Progress
+        {
+            get
+            {
+                return manager.Progress;
+            }
+        }
+
+        public TorrentManagerViewModelFacade(TorrentManager manager)
+        {
+            this.manager = manager;
+            manager.PieceHashed += Manager_PieceHashed;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Manager_PieceHashed(object sender, PieceHashedEventArgs e)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs("Progress"));
+            }
+        }
+    }
+}
---
+++ Tyranotorrent/Tyranotorrent/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MonoTorrent.Client;
+using MonoTorrent.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyranotorrent.ViewModels
+{
+    class MainWindowViewModel
+    {
+
+        public IEnumerable<TorrentManager> Downloads { get; set; }
+
+    }
+}
