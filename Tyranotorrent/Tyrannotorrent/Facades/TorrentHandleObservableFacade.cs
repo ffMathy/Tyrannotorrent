19348bb8afde11a5fca40b4a63040f9c72b2fb02
ffMathy <mathias.lorenzen@live.com>
2014-12-20 10:33:10 +0100

    fixed bug that would cause a crash sometimes.

--- Tyranotorrent/Tyrannotorrent/Facades/TorrentHandleObservableFacade.cs
+++ Tyranotorrent/Tyrannotorrent/Facades/TorrentHandleObservableFacade.cs
@@ -10,6 +10,7 @@ using System.IO;
 using Tyrannotorrent.Helpers;
 using System.Windows.Input;
 using Tyrannotorrent.Commands;
+using System.Runtime.InteropServices;
 
 namespace Tyrannotorrent.Facades
 {
@@ -171,7 +172,9 @@ namespace Tyrannotorrent.Facades
         {
             get
             {
-                return torrentHandle.TorrentFile == null ? "Loading ..." : torrentHandle.TorrentFile.Name;
+                try {
+                    return torrentHandle.TorrentFile == null ? "Loading ..." : torrentHandle.TorrentFile.Name;
+                } catch(ExternalException) { return "Error!"; }
             }
         }
 
